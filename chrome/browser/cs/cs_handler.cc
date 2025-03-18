741fc8b06d717200bd5f5af3284cc2528c70cdda
nyinyithann <nyinyithann@gmail.com>
2025-03-18 20:21:11 +0800

    clickstream: Enhanced search term detection by expanding the list of supported parameters (`q`, `p`, `wd`, `word`, `query`, `text`, `MT`, and `s`) and improved filtering logic accordingly.

--- chrome/browser/cs/cs_handler.cc
+++ chrome/browser/cs/cs_handler.cc
@@ -1,5 +1,30 @@
 #include "cs_handler.h"
 
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+
+namespace {
+std::unordered_map<std::string, std::string> parseQuery(
+    const std::string& query) {
+  std::unordered_map<std::string, std::string> params;
+  std::istringstream stream(query);
+  std::string pair;
+
+  while (std::getline(stream, pair, '&')) {
+    size_t equals_pos = pair.find('=');
+    if (equals_pos != std::string::npos) {
+      std::string key = pair.substr(0, equals_pos);
+      std::string value = pair.substr(equals_pos + 1);
+      params[key] = value;
+    }
+  }
+
+  return params;
+}
+}  // namespace
+
 namespace cs_handler {
 
 CSHandler::CSHandler(
@@ -14,15 +39,70 @@ void CSHandler::Handle(const GURL& url) {
     last_committed_url_ = url;
     if (url.SchemeIsHTTPOrHTTPS()) {
       std::string url_to_submit = url.spec();
-      const std::string query = url.query();
-      const bool has_q_param = query.find("q=");
-      const bool has_p_param = query.find("p=");
 
-      if (!(has_q_param || has_p_param)) {
-        GURL::Replacements remove_query;
-        remove_query.ClearQuery();
-        remove_query.ClearRef();
-        url_to_submit = url.ReplaceComponents(remove_query).spec();
+      GURL::Replacements remove_query;
+      remove_query.ClearQuery();
+      remove_query.ClearRef();
+      url_to_submit = url.ReplaceComponents(remove_query).spec();
+
+      const std::string query = url.query();
+      const std::string query_with_qm = std::string("?") + query;
+
+      constexpr std::size_t npos = std::string::npos;
+      const bool has_q_param = (query_with_qm.find("?q=") != npos) ||
+                               (query_with_qm.find("&q=") != npos);
+
+      const bool has_p_param = (query_with_qm.find("?p=") != npos) ||
+                               (query_with_qm.find("&p=") != npos);
+
+      const bool has_wd_or_word_param =
+          (query_with_qm.find("?wd=") != npos) ||
+          (query_with_qm.find("&wd=") != npos) ||
+          (query_with_qm.find("?word=") != npos) ||
+          (query_with_qm.find("&word=") != npos);
+
+      const bool has_query_param = (query_with_qm.find("?query=") != npos) ||
+                                   (query_with_qm.find("&query=") != npos);
+
+      const bool has_text_param = (query_with_qm.find("?text=") != npos) ||
+                                  (query_with_qm.find("&text=") != npos);
+
+      const bool has_mt_param = (query_with_qm.find("?MT=") != npos) ||
+                                (query_with_qm.find("&MT=") != npos);
+
+      const bool has_s_param = (query_with_qm.find("?s=") != npos) ||
+                               (query_with_qm.find("&s=") != npos);
+
+      const bool has_search_term_param =
+          has_q_param || has_p_param || has_wd_or_word_param ||
+          has_query_param || has_text_param || has_mt_param || has_s_param;
+
+      if (has_search_term_param) {
+        auto params = parseQuery(query);
+        if (has_q_param) {
+          url_to_submit += "?q=" + params["q"];
+        }
+        if (has_p_param) {
+          url_to_submit += "?p=" + params["p"];
+        }
+        if (has_wd_or_word_param) {
+          url_to_submit +=
+              "?wd=" + (params["wd"].length() > 0     ? params["wd"]
+                        : params["word"].length() > 0 ? params["word"]
+                                                      : "");
+        }
+        if (has_query_param) {
+          url_to_submit += "?query=" + params["query"];
+        }
+        if (has_text_param) {
+          url_to_submit += "?text=" + params["text"];
+        }
+        if (has_mt_param) {
+          url_to_submit += "?MT=" + params["MT"];
+        }
+        if (has_s_param) {
+          url_to_submit += "?s=" + params["s"];
+        }
       }
 
       api_client_->Post(
