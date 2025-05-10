8ad4d37d2d68bcc22363f015c2f4ba208aec0b65
nyinyithann <nyinyithann@gmail.com>
2025-05-10 17:12:31 +0800

    drm: DRM presence detection WIP

---
+++ chrome/common/widevine/constants.h
@@ -0,0 +1,7 @@
+#ifndef CHROMIUM_SRC_CHROME_COMMON_WIDEVINE_CONSTANTS_H_
+#define CHROMIUM_SRC_CHROME_COMMON_WIDEVINE_CONSTANTS_H_
+
+inline constexpr char kWidevineComponentId[] =
+    "oimompecagnajdejgnnjijobebaeigek";
+
+#endif //CHROMIUM_SRC_CHROME_COMMON_WIDEVINE_CONSTANTS_H_
