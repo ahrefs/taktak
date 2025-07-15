c6abffafa658524ba9275da956783496e3bc4030
nyinyithann <nyinyithann@gmail.com>
2025-07-16 06:22:55 +0800

    merge138: Improve response data checking logic

--- chrome/browser/ui/startup/update_notifier/update_notifier_prompt_manager.cc
+++ chrome/browser/ui/startup/update_notifier/update_notifier_prompt_manager.cc
@@ -89,13 +89,19 @@ void UpdateNotifierPromptManager::OnCheckNewerVersion(WebRequestResult result) {
              << result.response_code();
     return;
   }
-
+  DVLOG(0) << __func__
+           << " |>> Success checking new version with response code: "
+           << result.response_code();
   if (result.value_body().is_dict() ) {
     const base::Value::Dict &dict = result.value_body().GetDict();
     const std::optional<bool> success = dict.FindBool("success");
-    const std::optional<bool> data = dict.FindBool("data");
-    if (success.has_value() && success.value() && data.has_value() && data.value()) {
-      InitTabStripTracker();
+    DVLOG(0) << __func__ << " |>> success: " << success.value();
+    if (success.has_value() && success.value()) {
+      const std::optional<bool> data = dict.FindBool("data");
+      if (data.has_value() && data.value()) {
+        DVLOG(0) << __func__ << " |>> data: " << data.value();
+        InitTabStripTracker();
+      }
     }
   }
 }
