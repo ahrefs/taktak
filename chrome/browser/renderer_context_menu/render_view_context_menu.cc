136741807b66ded9c34aa8d411538a8d8b53f17a
nyinyithann <nyinyithann@gmail.com>
2025-08-30 20:38:58 +0800

    chore: disable google devices syncing service

--- chrome/browser/renderer_context_menu/render_view_context_menu.cc
+++ chrome/browser/renderer_context_menu/render_view_context_menu.cc
@@ -1044,6 +1044,7 @@ void RenderViewContextMenu::InitMenu() {
     AppendImageItems();
   }
 
+  /*
   if (content_type_->SupportsGroup(
           ContextMenuContentType::ITEM_GROUP_SEARCHWEBFORIMAGE)) {
     AppendSearchWebForImageItems();
@@ -1053,6 +1054,7 @@ void RenderViewContextMenu::InitMenu() {
           ContextMenuContentType::ITEM_GROUP_MEDIA_VIDEO)) {
     AppendVideoItems();
   }
+  */
 
   if (content_type_->SupportsGroup(
           ContextMenuContentType::ITEM_GROUP_MEDIA_AUDIO)) {
@@ -2140,12 +2142,13 @@ void RenderViewContextMenu::AppendPageItems() {
   // Note: `has_sharing_menu_items = true` also implies a separator was added
   // for sharing section.
   bool has_sharing_menu_items = false;
+
   // Send-Tab-To-Self (user's other devices), page level.
-  if (GetBrowser() &&
-      send_tab_to_self::ShouldDisplayEntryPoint(embedder_web_contents_)) {
-    AppendSendTabToSelfItem(/*add_separator=*/!has_sharing_menu_items);
-    has_sharing_menu_items = true;
-  }
+  // if (GetBrowser() &&
+  //    send_tab_to_self::ShouldDisplayEntryPoint(embedder_web_contents_)) {
+  //  AppendSendTabToSelfItem(/*add_separator=*/!has_sharing_menu_items);
+  //  has_sharing_menu_items = true;
+  // }
 
   // Context menu item for QR Code Generator.
   has_sharing_menu_items |=
@@ -3322,7 +3325,7 @@ void RenderViewContextMenu::ExecuteCommand(int id, int event_flags) {
       break;
 
     case IDC_SEND_TAB_TO_SELF:
-      send_tab_to_self::ShowBubble(embedder_web_contents_);
+      // send_tab_to_self::ShowBubble(embedder_web_contents_);
       break;
 
     case IDC_CONTENT_CONTEXT_GENERATE_QR_CODE: {
