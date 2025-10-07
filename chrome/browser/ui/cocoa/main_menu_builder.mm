5b2ae78641524ec68c16306b9233ff56fa17a66a
Nyi Nyi <nyinyi.than@ahrefs.com>
2025-10-07 20:59:16 +0800

    mac: stick to app.icns for product logo instead of newly introduced AppIcon

--- chrome/browser/ui/cocoa/main_menu_builder.mm
+++ chrome/browser/ui/cocoa/main_menu_builder.mm
@@ -543,29 +543,30 @@ NSMenuItem* BuildHelpMenu(NSApplication* nsapp,
                           id app_delegate,
                           const std::u16string& product_name,
                           bool is_pwa) {
-  // no help menu for Taktak
   return nil;
-//  if (is_pwa) {
-//    return nil;
-//  }
-//
-//  // clang-format off
-//  NSMenuItem* item =
-//      Item(IDS_HELP_MENU_MAC)
-//          .submenu({
-//#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
-//              Item(IDS_FEEDBACK_MAC)
-//                  .command_id(IDC_FEEDBACK),
-//#endif
-//              Item(IDS_HELP_MAC)
-//                  .string_format_1(product_name)
-//                  .command_id(IDC_HELP_PAGE_VIA_MENU),
-//          })
-//          .Build();
-//  // clang-format on
-//
-//  nsapp.helpMenu = item.submenu;
-//  return item;
+  /*
+  if (is_pwa) {
+    return nil;
+  }
+
+  // clang-format off
+  NSMenuItem* item =
+      Item(IDS_HELP_MENU_MAC)
+          .submenu({
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
+              Item(IDS_FEEDBACK_MAC)
+                  .command_id(IDC_FEEDBACK),
+#endif
+              Item(IDS_HELP_MAC)
+                  .string_format_1(product_name)
+                  .command_id(IDC_HELP_PAGE_VIA_MENU),
+          })
+          .Build();
+  // clang-format on
+
+  nsapp.helpMenu = item.submenu;
+  return item;
+  */
 }
 
 }  // namespace
